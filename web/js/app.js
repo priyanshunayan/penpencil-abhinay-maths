adba5a2a0a00b3591e937ccf34f0ee5940a2c620
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-10-16 15:23:18 +0530

    flow done

---
+++ web/js/app.js
@@ -0,0 +1,22 @@
+const enterNowBtn = document.getElementById('enterNow');
+const phoneInput = document.getElementById('phoneInput');
+const otpInput = document.getElementById('otpInput');
+const registerBtn = document.getElementById('registerBtn');
+const errMessage = document.getElementById('errMessage');
+
+enterNowBtn.addEventListener('click', () => {
+    if (phoneInput.value == 8986484443) {
+        enterNowBtn.setAttribute('data-target', '#login');
+    } else {
+        enterNowBtn.setAttribute('data-target', '#signup');
+        console.log("===========>", "This case");
+        registerBtn.addEventListener('click', () => {
+            if (otpInput.value == 1234) {
+                window.location.assign("https://abhinay.netlify.com/confirmPassword.html");
+            }
+            else {
+                errMessage.innerHTML = "Please input correct OTP";
+            }
+        })
+    }
+})
